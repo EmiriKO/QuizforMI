207aa08ab5261c4dbdbc5afda88d820b75f12ff9
hatatamami <yuritamaneko1167@gmail.com>
2021-09-16 16:25:43 +0900

    リスト表示

--- QuizforMI/app/src/main/java/events/tgh2021/quizformi/FlashcardActivity.java
+++ QuizforMI/app/src/main/java/events/tgh2021/quizformi/FlashcardActivity.java
@@ -3,12 +3,28 @@ package events.tgh2021.quizformi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class FlashcardActivity extends AppCompatActivity {
+
+    private static final String[] texts = {
+            "abc", "bcd", "cde"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
+
+
+
+        ListView listView = new ListView(this);
+        setContentView(listView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,texts);
+
+        listView.setAdapter(arrayAdapter);
+        
     }
 }
